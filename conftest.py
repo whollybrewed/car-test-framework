b40f198e47ac0346544661691cf054d75cf7e02a
whollybrewed <56617970+whollybrewed@users.noreply.github.com>
2024-07-08 21:53:20 +0200

    fix list

--- conftest.py
+++ conftest.py
@@ -1,13 +1,13 @@
 import pytest
 import glob
 
-quarantine_list = []
 def pytest_collection_modifyitems(items):
+    quarantine_list = []
     path = "quarantine*.txt"
     for filename in glob.glob(path):
         with open(filename, 'r') as f:
             quarantine_list = f.read().splitlines()
-
-    for item in items:
-        if item.nodeid in quarantine_list:
-            item.add_marker(pytest.mark.quarantine)
+    if quarantine_list is not None:
+        for item in items:
+            if item.nodeid in quarantine_list:
+                item.add_marker(pytest.mark.quarantine)
