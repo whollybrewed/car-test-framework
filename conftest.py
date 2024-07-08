bc94806fe87977cddb21fbb28fa0068f68289319
whollybrewed <56617970+whollybrewed@users.noreply.github.com>
2024-07-08 21:28:14 +0200

    Framework: add forgotten files

---
+++ conftest.py
@@ -0,0 +1,12 @@
+import pytest
+import glob
+
+def pytest_collection_modifyitems(items):
+    path = "quarantine*.txt"
+    for filename in glob.glob(path):
+        with open(filename, 'r') as f:
+            qlist = f.read().splitlines()
+
+    for item in items:
+        if item.nodeid in qlist:
+            item.add_marker(pytest.mark.quarantine)
